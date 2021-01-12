21e5e3a32da99061c1cb6b0dcfa9eb5f54295cfc
Maxim Krouk <40476363+maximkrouk@users.noreply.github.com>
2021-01-12 18:01:36 +0300

    Fix platform version error in `Package.swift` (#1)

--- Package.swift
+++ Package.swift
@@ -1,33 +1,37 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "TokamakPublish",
-  platforms: [.macOS(.v10_15)],
+  platforms: [.macOS(.v11)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "TokamakPublish",
       targets: ["TokamakPublish"]
     ),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
     .package(url: "https://github.com/JohnSundell/Publish.git", from: "0.7.0"),
     .package(url: "https://github.com/TokamakUI/Tokamak.git", .branch("main")),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "TokamakPublish",
-      dependencies: ["Publish", .product(name: "TokamakStaticHTML", package: "Tokamak")]
+      dependencies: [
+        .product(
+          name: "Publish",
+          package: "Publish"
+        ),
+        .product(
+          name: "TokamakStaticHTML",
+          package: "Tokamak"
+        )
+      ]
     ),
     .testTarget(
       name: "TokamakPublishTests",
-      dependencies: ["TokamakPublish"]
+      dependencies: [.target(name: "TokamakPublish")]
     ),
   ]
 )
